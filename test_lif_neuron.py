c6b1f75709e31b704fecbec15e1e206f23bfd150
joshdev1 <32490908+joshdev1@users.noreply.github.com>
2022-05-25 21:38:25 +0100

    updated test cases for new lif neuron class

--- test_lif_neuron.py
+++ test_lif_neuron.py
@@ -1,8 +1,14 @@
 import unittest
-from lif_neuron import lif_neuron
+from lif_neuron import LIFNeuron
 
 
 class TestLifNeuron(unittest.TestCase):
-    def test_lif_neuron(self):
-        _, rate = lif_neuron(5e-9, 0.1e-3, 1.5, 15e-3)
-        self.assertEqual(rate, 166)
+
+    def setUp(self) -> None:
+        self.neuron = LIFNeuron(5e-9, 0.1e-3, 1.5, 15e-3)
+
+    # TODO add mocks to test functions with no returns (neuron.run_neuron())
+
+    def test_get_rate(self):
+        self.neuron.run_neuron()
+        self.assertEqual(self.neuron.get_rate(), 166)
